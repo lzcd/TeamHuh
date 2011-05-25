f17db9aae72e7f7bc0430b185cc1aaa6be5e2f5f
lzcd <lzcd@hotmail.com>
2011-05-25 10:23:07 +1000

    Added solution

---
+++ TeamHuh/Query.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TeamHuh
+{
+    public class Query : DynamicObject, IEnumerable
+    {
+        private string baseUrl;
+        private string username;
+        private string password;
+
+        public Query(string baseUrl, string username, string password)
+        {
+            this.baseUrl = baseUrl;
+            this.username = username;
+            this.password = password;
+        }
+
+        private XDocument document;
+
+        protected Query(string baseUrl, string username, string password, XDocument document)
+            : this(baseUrl, username, password)
+        {
+            this.document = document;
+        }
+
+        WebClient client;
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var queryUrl = default(string);
+
+            if (document != null)
+            {
+
+                var selected = default(XElement);
+                if (TryFindDecendant(binder.Name, document, out selected))
+                {
+                    result = new Query(
+                        baseUrl: baseUrl,
+                        username: username,
+                        password: password,
+                        document: new XDocument(selected));
+                    return true;
+                }
+
+                if (selected == null)
+                {
+                    TryFindById(binder.Name, document, out selected);
+                }
+
+                if (selected == null)
+                {
+                    selected = document.Elements().First();
+                }
+
+
+                var attributeValue = default(string);
+                if (TryFindAttributeValueByName(binder.Name, selected, out attributeValue))
+                {
+                    result = attributeValue;
+                    return true;
+                }
+
+                var href = default(string);
+                TryFindAttributeValueByName("href", selected, out href);
+                queryUrl = baseUrl + href;
+            }
+            else
+            {
+                queryUrl = baseUrl + @"/httpAuth/app/rest/" + binder.Name.ToLower();
+            }
+
+            var childDocument = default(XDocument);
+            TryLoadXml(queryUrl, out childDocument);
+
+            result = new Query(
+                       baseUrl: baseUrl,
+                       username: username,
+                       password: password,
+                       document: childDocument);
+            return true;
+            //return base.TryGetMember(binder, out result);
+        }
+
+        private bool TryLoadXml(string queryUrl, out XDocument document)
+        {
+            if (client == null)
+            {
+                client = new WebClient();
+                client.Credentials = new NetworkCredential(username, password);
+                client.Headers.Add("Accepts:text/xml");
+            }
+
+            using (var stream = client.OpenRead(queryUrl))
+            using (var reader = XmlReader.Create(stream))
+            {
+                document = XDocument.Load(reader);
+            }
+
+            return true;
+        }
+
+        private bool TryFindDecendant(string name, XDocument document, out XElement selectedDecendant)
+        {
+            selectedDecendant = (from item in document.Descendants()
+                                 where item.Name.LocalName.Equals(name, StringComparison.CurrentCultureIgnoreCase)
+                                 select item).SingleOrDefault();
+
+            return (selectedDecendant != null);
+        }
+
+        private bool TryFindById(string name, XDocument document, out XElement selectedItem)
+        {
+            selectedItem = (from items in document.Elements()
+                            from item in items.Elements()
+                            from attribute in item.Attributes()
+                            where attribute.Name.LocalName.Equals("id", StringComparison.CurrentCultureIgnoreCase) &&
+                                  attribute.Value.Equals(name, StringComparison.CurrentCultureIgnoreCase)
+                            select item).SingleOrDefault();
+
+            return (selectedItem != null);
+        }
+
+        private bool TryFindAttributeValueByName(string name, XElement element, out string selectedAttributeValue)
+        {
+            var valueByName = (from attribute in element.Attributes()
+                               select new
+                               {
+                                   Key = attribute.Name.LocalName.ToLower(),
+                                   Value = attribute.Value
+                               }).ToDictionary(k => k.Key, v => v.Value);
+
+            return valueByName.TryGetValue(name.ToLower(), out selectedAttributeValue);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var decendants = from decendant in document.Elements().First().Descendants()
+                             select new Query(
+                               baseUrl: baseUrl,
+                               username: username,
+                               password: password,
+                               document: new XDocument(decendant));
+            return decendants.GetEnumerator();
+        }
+    }
+}
